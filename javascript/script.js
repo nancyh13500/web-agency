ffa1f5e1e039a2e8ab76ee86a232abcb8c8e97ad
nancyh13500 <nancy.hanoyan@free.fr>
2024-11-08 21:43:06 +0100

    update css

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,39 @@
-const image_width = 1920;
-const $prev = document.querySelector('.prev');
-const $next = document.querySelector('.next');
-const $imgBan = document.querySelector('.ban');
-const $images = document.querySelectorAll('.ban img');
-let currentImg = 1;
+const image_width = document.querySelector('.ban img').clientWidth;
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const imgBan = document.querySelector('.ban');
+const images = document.querySelectorAll('.ban img');
+// console.log(images);
+
+let currentImg = 1
+;
 let timeout;
 
 function updateImg() {
-    if (currentImg > $images.length) {
+    if (currentImg > images.length) {
         currentImg = 1;
     } else if (currentImg < 1) {
-        currentImg = $images.length - 1;
+        currentImg = images.length - 1;
     }
-    $imgBan.style.transform = `translateX(-${
+    imgBan.style.transform = `translateX(-${
         (currentImg - 1) * image_width}px)`;
         
         timeout = setTimeout(() => {
             currentImg++;
             updateImg();
-            // console.log(currentImg)
+            // console.log(currentImg);
+            
     }, 3000);
 }
 
-$prev.addEventListener ('click', () => {
+prev.addEventListener ('click', () => {
         clearTimeout(timeout);
         currentImg--;
         updateImg();
     },
 );
 
-$next.addEventListener ('click', () => {
+next.addEventListener ('click', () => {
         clearTimeout(timeout);
         currentImg++;
         updateImg();
