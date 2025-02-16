335e99a7519fd14d1a14f281f12d6d46bb696091
nancyh13500 <nancy.hanoyan@free.fr>
2025-02-16 18:08:22 +0100

    update css carousel

--- javascript/slide.js
+++ javascript/slide.js
@@ -1,44 +1,65 @@
-const image_width = document.querySelector('.ban img').clientWidth;
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
 const imgBan = document.querySelector('.ban');
 const images = document.querySelectorAll('.ban img');
-let currentImg = 0;
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 
-let timeout;
+let currentImg = 0;
+let imageWidth = images[0].clientWidth;
+
+window.addEventListener('resize', () => {
+    imageWidth = images[0].clientWidth;
+});
 
 function updateImg() {
-    if (currentImg > images.length) {
-        currentImg = 1;
-    } else if (currentImg < 1) {
+    if (currentImg >= images.length) {
+        currentImg = 0;
+    } else if (currentImg < 0) {
         currentImg = images.length - 1;
     }
-    imgBan.style.transform = `translateX(-${(currentImg - 1) * image_width}px)`;
 
-    timeout = setTimeout(() => {
-        currentImg++;
-        updateImg();
-
-    }, 5000);
+    imgBan.style.transform = `translateX(-${currentImg * imageWidth}px)`;
 }
 
 prev.addEventListener('click', () => {
-    clearTimeout(timeout);
     currentImg--;
     updateImg();
-},
-);
+});
 
 next.addEventListener('click', () => {
-    clearTimeout(timeout);
     currentImg++;
     updateImg();
-},
-);
+});
+
+// Ajustement du temps de transition automatique
+let autoSlide = setInterval(() => {
+    currentImg++;
+    updateImg();
+}, 5000);
+
+// Reset l'intervalle quand on clique
+prev.addEventListener('click', () => {
+    clearInterval(autoSlide);
+    autoSlide = setInterval(() => {
+        currentImg++;
+        updateImg();
+    }, 5000);
+});
+
+next.addEventListener('click', () => {
+    clearInterval(autoSlide);
+    autoSlide = setInterval(() => {
+        currentImg++;
+        updateImg();
+    }, 5000);
+});
+
+function resetAutoSlide() {
+    clearInterval(autoSlide);
+    autoSlide = setInterval(() => {
+        currentImg++;
+        updateImg();
+    }, 5000);
+}
+
+// Initialisation
 updateImg();
-
-
-
-
-
-
